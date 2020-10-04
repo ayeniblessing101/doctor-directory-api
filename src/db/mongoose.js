06fcc048c8ac4172bceca0da2f700524df8dd402
Blessing Ayeni <blessingayeni@Blessings-MacBook-Pro.local>
2020-10-04 19:14:25 +0100

    add sort and pagination

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 //mongodb://127.0.0.1:27017/doctor-directory-api
+// "mongodb://apotiEri:Welcome30#@ds115592.mlab.com:15592/doctor-directory-api"
 mongoose.connect(
   "mongodb://apotiEri:Welcome30#@ds115592.mlab.com:15592/doctor-directory-api",
   {
